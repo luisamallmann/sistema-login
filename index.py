bdbf16c3d4a4ddee1b20c4830d60cda3f9c6ca52
Luísa Mallmann <164959727+luisamallmann@users.noreply.github.com>
2026-05-29 09:03:19 -0300

    Add login route

--- index.py
+++ index.py
@@ -64,5 +64,10 @@ def cadastro():
         <button>Voltar para página inicial</button>
         </a>"""
 
+@app.route('/login',methods=['POST','GET'])
+def login():
+    if(request.method=='GET'):
+        return render_template('login.html')
+
 if __name__=='__main__':
     app.run(debug=True)
